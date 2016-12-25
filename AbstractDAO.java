0eab10fb1cc2bb4ba09f086b7a4c603219e678c0
vinnikoff <winr7post@gmail.com>
2016-12-25 20:52:07 +0200

    project8-1

---
+++ AbstractDAO.java
@@ -0,0 +1,12 @@
+/**
+ * Created by VINNI on 25.12.16.
+ */
+public interface AbstractDAO<T> {
+
+    T save(T);
+    void delete(T);
+    void deleteAll(List T);
+    void saveAll(List T);
+    List<T> getList();
+
+}
